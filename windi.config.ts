0343a13775654c5dc9a8e28f4ecc239fcd859a47
capt-nemo429 <nemo429@protonmail.com>
2021-12-21 20:29:09 +0000

    workaround windiccs animate-pulse bug

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "windicss/helpers";
 
 export default defineConfig({
+  shortcuts: {
+    skeleton: "bg-gray-300 animate-pulse inline-block"
+  },
   theme: {
     extend: {
       fontFamily: {
