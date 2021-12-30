591c7df544fca649573f78c979bf407b6fee535d
capt-nemo429 <nemo429@protonmail.com>
2021-12-30 20:43:50 +0000

    feed autocomplete componend with bip39 words

--- src/views/add/RestoreFromMnemonicView.vue
+++ src/views/add/RestoreFromMnemonicView.vue
@@ -43,45 +43,10 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import PageTitle from "@/components/PageTitle.vue";
+import * as bip39 from "bip39";
+import { take } from "lodash";
 
-const data = [
-  {
-    id: 1,
-    user: { first_name: "Jesse", last_name: "Simmons" },
-    date: "2016/10/15 13:43:27",
-    gender: "Male"
-  },
-  {
-    id: 2,
-    user: { first_name: "John", last_name: "Jacobs" },
-    date: "2016/12/15 06:00:53",
-    gender: "Male"
-  },
-  {
-    id: 3,
-    user: { first_name: "Tina", last_name: "Gilbert" },
-    date: "2016/04/26 06:26:28",
-    gender: "Female"
-  },
-  {
-    id: 4,
-    user: { first_name: "Clarence", last_name: "Flores" },
-    date: "2016/04/10 10:28:46",
-    gender: "Male"
-  },
-  {
-    id: 5,
-    user: { first_name: "Anne", last_name: "Lee" },
-    date: "2016/12/06 14:38:38",
-    gender: "Female"
-  },
-  {
-    id: 6,
-    user: { first_name: "Sara", last_name: "Armstrong" },
-    date: "2016/09/23 18:50:04",
-    gender: "Female"
-  }
-];
+const data = bip39.wordlists.english;
 
 export default defineComponent({
   name: "RestoreFromMnemonic",
@@ -99,9 +64,12 @@ export default defineComponent({
   },
   methods: {
     getFilteredTags(text: string) {
-      this.filteredTags = data.filter(option => {
-        return option.user.first_name.toString().toLowerCase().indexOf(text.toLowerCase()) >= 0;
-      });
+      this.filteredTags = take(
+        data.filter(option => {
+          return option.indexOf(text.toLowerCase()) >= 0;
+        }),
+        10
+      );
     }
   }
 });
