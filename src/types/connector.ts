6285349c1d8c49cc088bb49e614cc39238830583
capt-nemo429 <nemo429@protonmail.com>
2022-01-30 18:21:46 -0300

    add signing types

--- src/types/connector.ts
+++ src/types/connector.ts
@@ -46,3 +46,60 @@ export type APIError = {
   code: APIErrorCode;
   info: string;
 };
+
+export enum TxSendErrorCode {
+  Refused = 1,
+  Failure = 2
+}
+
+export type TxSendError = {
+  code: TxSendErrorCode;
+  info: String;
+};
+
+export enum TxSignErrorCode {
+  ProofGeneration = 1,
+  UserDeclined = 2
+}
+
+export type TxSignError = {
+  code: TxSignErrorCode;
+  info: String;
+};
+
+export type Token = {
+  tokenId: string;
+  amount: number | string;
+  name?: string;
+  decimals?: number;
+};
+
+export type UnsignedInput = {
+  boxId: string;
+  transactionId: string;
+  index: number;
+  ergoTree: string;
+  creationHeight: number;
+  value: number | string;
+  assets: Token[];
+  additionalRegisters: { [key: string]: string };
+  extension: { [key: string]: string };
+};
+
+export type ErgoBoxCandidate = {
+  readonly value: number | string;
+  readonly ergoTree: string;
+  readonly creationHeight: number;
+  readonly assets: Token[];
+  readonly additionalRegisters: { [key: string]: string };
+};
+
+export type DataInput = {
+  boxId: string;
+};
+
+export type UnsignedTx = {
+  inputs: UnsignedInput[];
+  dataInputs: DataInput[];
+  outputs: ErgoBoxCandidate[];
+};
