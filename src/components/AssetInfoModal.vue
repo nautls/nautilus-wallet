f1385d6a0d647038146a3ddb245587bfdc445f54
capt-nemo429 <nemo429@protonmail.com>
2022-03-24 21:03:32 -0300

    add nft displaying

--- src/components/AssetInfoModal.vue
+++ src/components/AssetInfoModal.vue
@@ -3,19 +3,29 @@
     v-model:active="active"
     :auto-focus="true"
     :can-cancel="true"
-    @onClose="emitOnClose()"
     scroll="clip"
-    content-class="max-h-90vh bg-transparent overflow-hidden !w-90vw"
+    content-class="max-h-90vh bg-transparent overflow-hidden !max-w-110 !w-90vw"
     overlay-class="opacity-40"
   >
-    <div class="h-10"></div>
-    <div class="pt-10 text-xs tracking-normal rounded bg-light-50">
+    <div class="h-10" v-if="!isImageNft" @click="active = false"></div>
+    <div class="text-xs tracking-normal rounded bg-light-50" :class="{ 'pt-10': !isImageNft }">
+      <iframe
+        v-if="isImageNft"
+        sandbox=""
+        class="h-80 w-full rounded-t m-0 p-0"
+        :src="contentUrl"
+        frameborder="0"
+      ></iframe>
       <asset-icon
+        v-else
         class="w-20 h-20 mx-auto absolute left-0 right-0 top-0"
         :token-id="asset?.id"
         :type="asset?.subtype"
       />
-      <div class="p-4 flex flex-col gap-4 overflow-x-hidden overflow-y-auto max-h-70vh">
+      <div
+        class="p-4 gap-4 flex flex-col overflow-x-hidden overflow-y-auto"
+        :class="isImageNft ? 'max-h-50' : 'max-h-100'"
+      >
         <div>
           <h1 class="font-bold text-lg">
             {{ asset?.name ?? $filters.compactString(asset?.id, 20) }}
@@ -60,6 +70,24 @@ import { assetInfoDbService } from "@/api/database/assetInfoDbService";
 import { ERG_TOKEN_ID } from "@/constants/ergo";
 import { isEmpty } from "lodash";
 import { decimalize, toBigNumber } from "@/utils/bigNumbers";
+import { AssetSubtype } from "@/types/internal";
+
+const CONTENT_SANDBOX_URL = "https://localhost:7059";
+function resolveIpfs(url?: string, isVideo = false): string {
+  if (!url) {
+    return "";
+  }
+
+  const ipfsPrefix = "ipfs://";
+  if (!url.startsWith(ipfsPrefix)) {
+    return url;
+  } else {
+    if (isVideo) {
+      return url.replace(ipfsPrefix, "https://ipfs.blockfrost.dev/ipfs/");
+    }
+    return url.replace(ipfsPrefix, "https://cloudflare-ipfs.com/ipfs/");
+  }
+}
 
 export default defineComponent({
   name: "AssetInfoModal",
@@ -78,6 +106,17 @@ export default defineComponent({
       }
 
       return this.$filters.formatBigNumber(n, undefined, Number.MAX_SAFE_INTEGER);
+    },
+    isImageNft(): boolean {
+      return this.asset?.subtype === AssetSubtype.PictureArtwork;
+    },
+    contentUrl(): string | undefined {
+      if (!this.asset?.artworkUrl && !this.asset?.artworkCover) {
+        return;
+      }
+
+      const url = this.asset.artworkUrl ?? this.asset.artworkCover;
+      return `${CONTENT_SANDBOX_URL}/?url=${resolveIpfs(url)}`;
     }
   },
   data() {
@@ -95,6 +134,13 @@ export default defineComponent({
 
       this.getAssetInfo();
       this.active = true;
+    },
+    active(newVal: boolean) {
+      if (newVal) {
+        return;
+      }
+
+      this.emitOnClose();
     }
   },
   methods: {
