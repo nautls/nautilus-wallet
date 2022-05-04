dd0f25071602f68e592386b312e6d8e8f91505ce
capt-nemo429 <nemo429@protonmail.com>
2022-05-04 15:23:52 -0300

    adjust device screeen positioning

--- src/components/LedgerDevice.vue
+++ src/components/LedgerDevice.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex gap-2 flex-col mx-auto items-center min-w-62 max-w-65 w-65">
+  <div class="flex gap-2 flex-col mx-auto items-center min-w-72 max-w-75 w-75 p-4">
     <div
       v-if="connected || (state !== 'unknown' && state !== 'deviceNotFound')"
       class="w-auto mx-auto text-center text-sm"
     >
       <div class="relative">
-        <img v-if="isNanoX" src="@/assets/images/hw-devices/ledger-x.svg" class="w-60" />
-        <img v-else src="@/assets/images/hw-devices/ledger-s.svg" class="w-55" />
-        <div :class="screenPosition" class="absolute text-light-500 text-xs items-center">
+        <ledger-nano-x v-if="isNanoX" class="w-max" />
+        <ledger-nano-s v-else class="w-max" />
+        <div :class="screenPosition" class="absolute text-light-600 text-xs items-center">
           <div class="flex h-full items-center">
             <div class="w-full h-auto">
               <p v-if="state === 'success'">
@@ -47,9 +47,15 @@
 <script lang="ts">
 import { LedgerDeviceModelId } from "@/constants/ledger";
 import { defineComponent } from "vue";
+import ledgerNanoS from "@/assets/images/hw-devices/ledger-s.svg";
+import ledgerNanoX from "@/assets/images/hw-devices/ledger-x.svg";
 
 export default defineComponent({
   name: "LedgerDevice",
+  components: {
+    ledgerNanoS,
+    ledgerNanoX
+  },
   props: {
     state: { type: String, required: false },
     screenText: { type: String, required: false },
@@ -62,9 +68,9 @@ export default defineComponent({
   computed: {
     screenPosition() {
       if (this.isNanoX) {
-        return "top-18 left-14 w-28.5 h-8";
+        return "top-19.1 left-15.1 w-28.5 h-8";
       } else {
-        return "top-17.5 left-7 w-28.5 h-8";
+        return "top-19.5 left-8.3 w-30 h-8";
       }
     },
     isNanoX() {
