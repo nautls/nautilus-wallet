71eebdfd376618cd1b1864c9781cffb5d2a7f045
capt-nemo429 <nemo429@protonmail.com>
2022-07-20 12:01:24 -0300

    improve message build docs

--- src/api/ergo/eip28.ts
+++ src/api/ergo/eip28.ts
@@ -2,7 +2,7 @@
  * Creates a EIP-28 signing message
  * @param message
  * @param origin
- * @returns the signing message formatted as "signingMessage;origin;timestamp;randomBytes"
+ * @returns the signing message formatted as "signingMessage;origin;timestamp;randomValue"
  */
 export function buildEip28ResponseMessage(message: string, origin: string): string {
   return `${message};${origin};${Math.floor(Date.now() / 1000)};${crypto.randomUUID()}`;
