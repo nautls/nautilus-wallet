fb9f3bd82ff30d594fbb534fcc71e81b7aa167c3
capt-nemo429 <nemo429@protonmail.com>
2022-07-20 13:43:20 -0300

    improve randomBytes generation

--- src/api/ergo/eip28.ts
+++ src/api/ergo/eip28.ts
@@ -2,8 +2,10 @@
  * Creates a EIP-28 signing message
  * @param message
  * @param origin
- * @returns the signing message formatted as "signingMessage;origin;timestamp;randomValue"
+ * @returns the signing message formatted as "signingMessage;origin;timestamp;randomBytes"
  */
 export function buildEip28ResponseMessage(message: string, origin: string): string {
-  return `${message};${origin};${Math.floor(Date.now() / 1000)};${crypto.randomUUID()}`;
+  return `${message};${origin};${Math.floor(Date.now() / 1000)};${crypto
+    .randomUUID()
+    .replaceAll("-", "")}`;
 }
