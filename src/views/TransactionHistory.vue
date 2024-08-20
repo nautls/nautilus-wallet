415f75366baca53d4beebcfabfe8a153ade6cddb
arobsn <87387688+arobsn@users.noreply.github.com>
2024-08-20 20:17:29 -0300

    set initial structure

---
+++ src/views/TransactionHistory.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  ChainProviderConfirmedTransaction,
+  ChainProviderUnconfirmedTransaction
+} from "@fleet-sdk/blockchain-providers";
+import { onMounted, shallowRef } from "vue";
+import { graphQLService } from "@/chains/ergo/services/graphQlService";
+import { useWalletStore } from "@/stores/walletStore";
+
+const wallet = useWalletStore();
+
+const confirmed = shallowRef<ChainProviderConfirmedTransaction<string>[]>([]);
+const pending = shallowRef<ChainProviderUnconfirmedTransaction<string>[]>([]);
+
+onMounted(async () => {
+  const addresses = wallet.addresses.map((x) => x.script);
+  pending.value = await graphQLService.getUnconfirmedTransactions({ where: { addresses } });
+  for await (const data of graphQLService.streamConfirmedTransactions({ where: { addresses } })) {
+    confirmed.value = data;
+    break;
+  }
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">tx history {{ confirmed }}</div>
+</template>
