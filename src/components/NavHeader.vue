951d36dd6035f1a0c6b15e20a94a17850b9355fa
arobsn <87387688+arobsn@users.noreply.github.com>
2024-12-17 09:49:23 -0300

    migrate NavHeader icons

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import { computed } from "vue";
+import {
+  ChartPieIcon,
+  ClockIcon,
+  DownloadIcon,
+  ImageIcon,
+  LayoutGridIcon,
+  SendIcon
+} from "lucide-vue-next";
 import { WalletType } from "@/types/internal";
 import { useWalletStore } from "@/stores/walletStore";
 
 const wallet = useWalletStore();
+const iconSize = 22;
 
 const readonly = computed(() => wallet.type === WalletType.ReadOnly);
 const containsArtwork = computed(() => wallet.artworkBalance.length > 0);
@@ -14,32 +23,32 @@ const containsArtwork = computed(() => wallet.artworkBalance.length > 0);
     <div class="tab-item spacing"></div>
     <router-link to="/" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="Assets">
-        <vue-feather type="pie-chart" class="m-3" size="22" />
+        <chart-pie-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <router-link v-if="containsArtwork" to="/nft" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="NFT gallery">
-        <vue-feather type="image" class="m-3" size="22" />
+        <image-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <router-link to="/history" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="History">
-        <vue-feather type="clock" class="m-3" size="22" />
+        <clock-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <router-link to="/receive" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="Receive">
-        <vue-feather type="download" class="m-3" size="22" />
+        <download-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <router-link v-if="!readonly" to="/send" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="Send">
-        <vue-feather type="send" class="m-3" size="22" />
+        <send-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <router-link to="/dapps" active-class="active" class="w-full tab-item">
       <tool-tip position="bottom" label="dApps">
-        <vue-feather type="grid" class="m-3" size="22" />
+        <layout-grid-icon class="m-3" :size="iconSize" />
       </tool-tip>
     </router-link>
     <div class="tab-item spacing"></div>
