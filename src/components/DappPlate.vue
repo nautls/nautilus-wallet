e6b9ca2352b30267dec2b05b02b6e67ab8e5c3a4
arobsn <87387688+arobsn@users.noreply.github.com>
2024-12-17 10:17:36 -0300

    migrate `WalletHeader` icons

--- src/components/DappPlate.vue
+++ src/components/DappPlate.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed } from "vue";
+import { CircleHelpIcon } from "lucide-vue-next";
 
 const props = defineProps({
   origin: { type: String, default: "" },
@@ -15,7 +16,7 @@ const domain = computed(() => props.origin) || "???";
       class="mx-auto rounded-full ring-1 ring-offset-1 ring-offset-gray-50 ring-gray-300 w-11 h-11"
     >
       <img v-if="favicon" :src="favicon" class="rounded-full w-11 h-11" />
-      <vue-feather v-else type="help-circle" class="text-gray-500 w-11 h-11" />
+      <circle-help-icon v-else class="text-gray-500 w-11 h-11" />
     </div>
     <p v-if="domain" class="text-gray-600">{{ domain }}</p>
   </div>
