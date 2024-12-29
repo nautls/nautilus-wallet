b423b23f1e60f7e4571693ffd3d60a761fbecb8d
arobsn <87387688+arobsn@users.noreply.github.com>
2024-12-29 12:26:23 -0300

    add `Link` component

---
+++ src/components/ui/link/Link.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { HtmlHTMLAttributes } from "vue";
+import { cn } from '@/lib/utils'
+
+const props = withDefaults(
+  defineProps<{
+    href: string;
+    class?: HtmlHTMLAttributes["class"];
+    external: boolean;
+  }>(),
+  {
+    external: false
+  }
+);
+</script>
+
+<template>
+  <a
+    :class="cn(props.class, 'text-primary underline-offset-4 hover:underline')"
+    :href="props.href"
+    :target="external ? '_blank' : undefined"
+    :rel="external ? 'noopener noreferrer' : undefined"
+  >
+    <slot />
+  </a>
+</template>
