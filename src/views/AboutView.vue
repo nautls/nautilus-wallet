5d4659d81b50eecfd70ca4c25f3eeac3e5bb95a2
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-11 23:43:11 -0300

    refactor `AboutView`

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,56 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import { useWalletStore } from "@/stores/walletStore";
+import { Button } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
+import { useFormat } from "@/composables/useFormat";
+import { MAINNET } from "@/constants/ergo";
+import { WalletType } from "@/types/internal";
+import pkg from "../../package.json";
+
+const wallet = useWalletStore();
+const format = useFormat();
+const router = useRouter();
+
+const version = pkg.version;
+const gitHash = import.meta.env.GIT_COMMIT_HASH;
+const shortGitHash = gitHash.slice(0, 7);
+const commitUrl = `https://github.com/capt-nemo429/nautilus-wallet/commit/${gitHash}`;
+const donationAddress = "9iPgSVU3yrRnTxtJC6hYA7bS5mMqZtjeJHrT3fNdLV7JZVpY5By";
+const readonly = computed(() => wallet.type === WalletType.ReadOnly);
+
+function goDonate() {
+  router.push({ name: "send-page", query: { recipient: donationAddress } });
+}
+</script>
+
 <template>
-  <div class="flex h-full flex-col gap-4 py-4 text-center text-sm">
+  <div class="flex h-full flex-col gap-4 p-4 text-center text-sm">
     <img src="/icons/app/logo-mainnet.svg?url" class="m-auto w-24 pt-2" />
     <div>
       <h1 v-once class="m-auto text-2xl">
-        <span v-if="testnet">Nautilus Testnet Wallet</span>
-        <span v-else>Nautilus Wallet</span>
+        <span v-if="MAINNET">Nautilus Wallet</span>
+        <span v-else>Nautilus Testnet Wallet</span>
       </h1>
 
       <p v-once>
         v{{ version }}, Commit:
-        <a :href="commitUrl" target="_blank" class="url">{{ shortGitHash }}</a>
+        <Link :href="commitUrl" external>{{ shortGitHash }}</Link>
       </p>
     </div>
-    <p class="text-sm italic text-gray-600">
+    <p class="text-sm italic text-muted-foreground">
       Built-in secrecy, sourcing parts from unnamed sources. Roams the seas beyond the reach of
       land-based governments.
     </p>
 
     <div class="flex-grow"></div>
 
-    <a
-      class="url m-auto text-sm"
+    <Link
+      class="m-auto text-sm"
       target="_blank"
       href="https://github.com/nautls/nautilus-wallet/blob/master/privacy-policy.md"
-      >Privacy Policy</a
+      >Privacy Policy</Link
     >
 
     <p class="text-sm">
@@ -32,77 +59,48 @@
         {{ format.string.shorten(donationAddress, 20) }}
         <click-to-copy :content="donationAddress" :size="14" class="align-middle" />
       </span>
-      <a v-else class="url cursor-pointer font-mono" @click="goDonate()">{{
+      <Button v-else variant="link" @click="goDonate()">{{
         format.string.shorten(donationAddress, 20)
-      }}</a>
+      }}</Button>
     </p>
 
     <div class="mx-20 flex flex-row items-center gap-1">
-      <a class="m-auto" target="_blank" href="https://github.com/capt-nemo429/nautilus-wallet"
-        ><svg role="img" class="h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <title>GitHub</title>
+      <Link class="m-auto" external href="https://github.com/capt-nemo429/nautilus-wallet"
+        ><svg
+          fill="currentColor"
+          role="img"
+          class="h-5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
           /></svg
-      ></a>
-      <a class="m-auto" target="_blank" href="https://twitter.com/NautilusWallet"
-        ><svg role="img" class="h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <title>X</title>
+      ></Link>
+      <Link class="m-auto" external href="https://twitter.com/NautilusWallet"
+        ><svg
+          fill="currentColor"
+          role="img"
+          class="h-5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
           /></svg
-      ></a>
-      <a class="m-auto" target="_blank" href="https://t.me/nautiluswallet"
-        ><svg role="img" class="h-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <title>Telegram</title>
+      ></Link>
+      <Link class="m-auto" external href="https://t.me/nautiluswallet"
+        ><svg
+          fill="currentColor"
+          role="img"
+          class="h-5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"
           /></svg
-      ></a>
+      ></Link>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import { useWalletStore } from "@/stores/walletStore";
-import { useFormat } from "@/composables/useFormat";
-import { MAINNET } from "@/constants/ergo";
-import { WalletType } from "@/types/internal";
-import pkg from "../../package.json";
-
-export default defineComponent({
-  name: "AboutView",
-  setup() {
-    return { wallet: useWalletStore(), format: useFormat() };
-  },
-  computed: {
-    version(): string {
-      return pkg.version;
-    },
-    gitHash(): string {
-      return import.meta.env.GIT_COMMIT_HASH;
-    },
-    shortGitHash(): string {
-      return this.gitHash?.slice(0, 7) ?? "";
-    },
-    commitUrl(): string {
-      return `https://github.com/capt-nemo429/nautilus-wallet/commit/${this.gitHash}`;
-    },
-    donationAddress(): string {
-      return "9iPgSVU3yrRnTxtJC6hYA7bS5mMqZtjeJHrT3fNdLV7JZVpY5By";
-    },
-    readonly(): boolean {
-      return this.wallet.type === WalletType.ReadOnly;
-    },
-    testnet() {
-      return !MAINNET;
-    }
-  },
-  methods: {
-    goDonate() {
-      this.$router.push({ name: "send-page", query: { recipient: this.donationAddress } });
-    }
-  }
-});
-</script>
