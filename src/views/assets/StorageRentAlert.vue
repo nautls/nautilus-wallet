f914303d8c33435ab9290e04a4f8f9736e5e1427
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-14 17:38:51 -0300

    use router directly to navigate to opt dapp

--- src/views/assets/StorageRentAlert.vue
+++ src/views/assets/StorageRentAlert.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router";
 import { useWalletStore } from "@/stores/walletStore";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { Link } from "@/components/ui/link";
 import { HEALTHY_UTXO_COUNT } from "@/constants/ergo";
 
-const wallet = useWalletStore();
 const STORAGE_RENT_URL = "https://ergoplatform.org/en/blog/2022-02-18-ergo-explainer-storage-rent/";
+
+const wallet = useWalletStore();
+const router = useRouter();
+
+const goToOptimizationDapp = () => router.push({ name: "wallet-optimization" });
 </script>
 
 <template>
-  <Alert v-if="!wallet.health.hasOldUtxos" variant="destructive">
+  <Alert v-if="wallet.health.hasOldUtxos" variant="destructive">
     <AlertTitle v-once
       >You may soon incur <Link external :href="STORAGE_RENT_URL">demurrage</Link>
     </AlertTitle>
@@ -20,9 +25,7 @@ const STORAGE_RENT_URL = "https://ergoplatform.org/en/blog/2022-02-18-ergo-expla
       Consolidate your assets to avoid this.
     </AlertDescription>
 
-    <router-link v-slot="{ navigate }" class="mt-4" to="/dapps/wallet-optimization" custom>
-      <Button class="w-full" @click="navigate">Consolidate</Button>
-    </router-link>
+    <Button class="w-full mt-4" @click="goToOptimizationDapp">Consolidate</Button>
   </Alert>
 
   <Alert v-else-if="wallet.health.utxoCount > HEALTHY_UTXO_COUNT">
@@ -32,8 +35,6 @@ const STORAGE_RENT_URL = "https://ergoplatform.org/en/blog/2022-02-18-ergo-expla
       Consider optimizing your wallet for improved performance and efficiency.
     </AlertDescription>
 
-    <router-link v-slot="{ navigate }" class="mt-4" to="/dapps/wallet-optimization" custom>
-      <Button class="w-full" @click="navigate">Optimize</Button>
-    </router-link>
+    <Button class="w-full mt-4" @click="goToOptimizationDapp">Optimize</Button>
   </Alert>
 </template>
