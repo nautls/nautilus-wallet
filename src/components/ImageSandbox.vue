fbde13aa005aa0b06d5fbde704db9ed83aaa22a1
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-16 15:32:00 -0300

    add external link button

--- src/components/ImageSandbox.vue
+++ src/components/ImageSandbox.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, HTMLAttributes, ref, watch } from "vue";
-import { CircleAlertIcon, LoaderCircleIcon } from "lucide-vue-next";
+import { CircleAlertIcon, ExternalLinkIcon, LoaderCircleIcon } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
 import {
   CONTENT_SANDBOX_URL,
   IPFS_GENERAL_GATEWAY,
@@ -14,11 +15,15 @@ const props = defineProps<{
   height?: string;
   objectFit?: string;
   overflow?: string;
+  displayExternalLink?: boolean;
 }>();
 
 const loading = ref(true);
 
-const contentUrl = computed(() => {
+const content = computed(() => resolveIpfs(props.src));
+const isContentUrl = computed(() => content.value?.startsWith("http"));
+
+const sandboxUrl = computed(() => {
   if (!props.src) return;
 
   let query = "";
@@ -26,7 +31,7 @@ const contentUrl = computed(() => {
   if (props.objectFit) query += `&fit=${props.objectFit}`;
   if (props.overflow) query += `&overflow=${props.overflow}`;
 
-  return `${CONTENT_SANDBOX_URL}/?url=${encodeURIComponent(resolveIpfs(props.src))}${query}`;
+  return `${CONTENT_SANDBOX_URL}/?url=${encodeURIComponent(content.value)}${query}`;
 });
 
 watch(
@@ -35,6 +40,10 @@ watch(
     loading.value = true;
   }
 );
+
+function openLink(url: string | undefined) {
+  window.open(url, "_blank");
+}
 
 function resolveIpfs(url?: string, isVideo = false): string {
   if (!url) return "";
@@ -48,17 +57,30 @@ function resolveIpfs(url?: string, isVideo = false): string {
   <div v-if="loading" :class="props.class" class="flex">
     <LoaderCircleIcon type="circular" class="animate-spin m-auto size-1/3 text-muted-foreground" />
   </div>
-  <div v-else-if="!contentUrl" :class="props.class" class="flex">
+  <div v-else-if="!sandboxUrl" :class="props.class" class="flex">
     <circle-alert-icon class="m-auto text-orange-400" :size="48" />
   </div>
-  <iframe
-    v-show="!loading && contentUrl"
-    :key="contentUrl"
-    :src="contentUrl"
-    sandbox=""
-    :class="props.class"
-    class="m-0 p-0"
-    frameborder="0"
-    @load="loading = false"
-  ></iframe>
+
+  <div class="relative">
+    <iframe
+      v-show="!loading && sandboxUrl"
+      :key="sandboxUrl"
+      :src="sandboxUrl"
+      sandbox=""
+      :class="props.class"
+      class="m-0 p-0"
+      frameborder="0"
+      @load="loading = false"
+    ></iframe>
+
+    <Button
+      v-if="props.displayExternalLink && isContentUrl"
+      class="absolute top-0 right-0 m-2 hover:opacity-100 opacity-70 transition-all"
+      size="icon"
+      variant="outline"
+      @click="openLink(content)"
+    >
+      <ExternalLinkIcon />
+    </Button>
+  </div>
 </template>
