5dfc2a410d8fe7c62843f237f490a6a71adfa2be
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-19 19:20:25 -0300

    add icon support

--- src/components/ui/separator/Separator.vue
+++ src/components/ui/separator/Separator.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
 import { Separator, type SeparatorProps } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import { type Component, computed, type HTMLAttributes } from 'vue'
 
 const props = defineProps<
-  SeparatorProps & { class?: HTMLAttributes['class'], label?: string }
+  SeparatorProps & { class?: HTMLAttributes['class'], label?: string, icon?: Component }
 >()
 
 const delegatedProps = computed(() => {
@@ -26,13 +26,17 @@ const delegatedProps = computed(() => {
     "
   >
     <span
-      v-if="props.label"
+      v-if="props.label || props.icon"
       :class="
         cn(
           'text-xs text-muted-foreground bg-background absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center',
           props.orientation === 'vertical' ? 'w-[1px] px-1 py-2' : 'h-[1px] py-1 px-2',
+          props.icon ? 'size-auto p-2 rounded-full border' : '',
         )
       "
-    >{{ props.label }}</span>
+    >
+      <Component :is="props.icon" v-if="props.icon" class="size-4" /> 
+      <template v-else>{{ props.label }}</template>
+    </span>
   </Separator>
 </template>
