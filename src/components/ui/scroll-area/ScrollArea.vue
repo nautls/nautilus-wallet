705588bedef946e6935f88ada62f7d57d1c40f3d
arobsn <87387688+arobsn@users.noreply.github.com>
2025-01-21 10:24:11 -0300

    add hide outline

--- src/components/ui/scroll-area/ScrollArea.vue
+++ src/components/ui/scroll-area/ScrollArea.vue
@@ -9,7 +9,7 @@ import {
 import { computed, type HTMLAttributes } from 'vue'
 import ScrollBar from './ScrollBar.vue'
 
-const props = defineProps<ScrollAreaRootProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<ScrollAreaRootProps & { class?: HTMLAttributes['class'], hideOutline?: boolean }>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
@@ -20,7 +20,7 @@ const delegatedProps = computed(() => {
 
 <template>
   <ScrollAreaRoot v-bind="delegatedProps" :class="cn('relative overflow-hidden', props.class)">
-    <ScrollAreaViewport class="h-full w-full rounded-[inherit]">
+    <ScrollAreaViewport :class="cn('h-full w-full rounded-[inherit]', { 'outline-none': props.hideOutline })">
       <slot />
     </ScrollAreaViewport>
     <ScrollBar />
