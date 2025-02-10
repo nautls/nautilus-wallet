dcf89b64407f14fe03d87c2353a6af03e8c5b91c
arobsn <87387688+arobsn@users.noreply.github.com>
2025-02-10 10:42:27 -0300

    add `PasswordInput` component
    Fixes Add 'View' password button #41

---
+++ src/components/ui/input/PasswordInput.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { HTMLAttributes, ref } from "vue";
+import { EyeIcon, EyeOffIcon } from "lucide-vue-next";
+import { Button } from "../button";
+import { Input } from ".";
+
+const props = defineProps<{
+  id?: HTMLAttributes["id"];
+}>();
+
+const showPassword = ref(false);
+</script>
+
+<template>
+  <div class="relative w-full items-center">
+    <Input
+      :id="props.id"
+      v-bind="$attrs"
+      :type="showPassword ? 'text' : 'password'"
+      placeholder="Search..."
+      class="pr-8"
+    />
+    <Button
+      size="condensed"
+      variant="minimal"
+      class="absolute cursor-default end-0 inset-y-0 flex text-muted-foreground items-center h-full justify-center pr-3 pl-1 [&_svg]:size-4"
+      @click="showPassword = !showPassword"
+    >
+      <EyeIcon v-if="showPassword" />
+      <EyeOffIcon v-else />
+    </Button>
+  </div>
+</template>
