fe52b493bbd0120de65a198ed3473601693669ed
arobsn <87387688+arobsn@users.noreply.github.com>
2025-02-21 09:58:59 -0300

    set global as default settings page

--- src/views/settings/SettingsView.vue
+++ src/views/settings/SettingsView.vue
@@ -8,16 +8,16 @@ import WalletSettings from "./wallet-settings/WalletSettings.vue";
 
 <template>
   <ScrollArea type="scroll">
-    <Tabs default-value="wallet-settings" class="w-full px-4 pt-6">
+    <Tabs default-value="global-settings" class="w-full px-4 pt-6">
       <TabsList class="flex w-full">
-        <TabsTrigger class="w-full" value="wallet-settings">Wallet</TabsTrigger>
         <TabsTrigger class="w-full" value="global-settings">Global</TabsTrigger>
+        <TabsTrigger class="w-full" value="wallet-settings">Wallet</TabsTrigger>
         <TabsTrigger class="w-full" value="connections">Connections</TabsTrigger>
       </TabsList>
 
       <div class="py-4">
-        <TabsContent value="wallet-settings"><WalletSettings /></TabsContent>
         <TabsContent value="global-settings"><GlobalSettings /></TabsContent>
+        <TabsContent value="wallet-settings"><WalletSettings /></TabsContent>
         <TabsContent value="connections"><ConnectedApps /> </TabsContent>
       </div>
     </Tabs>
