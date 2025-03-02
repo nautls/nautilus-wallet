a91313a20af22e36e874ddedb096901cf8da2ee5
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-02 13:37:44 -0300

    improve navbar reactivity

--- src/extension/popup/components/NavHeader.vue
+++ src/extension/popup/components/NavHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { ChartPieIcon, ClockIcon, DownloadIcon, LayoutGridIcon, SendIcon } from "lucide-vue-next";
 import { useWalletStore } from "@/stores/walletStore";
 import { WalletType } from "@/types/internal";
@@ -7,13 +7,13 @@ import { WalletType } from "@/types/internal";
 const wallet = useWalletStore();
 
 const readonly = computed(() => wallet.type === WalletType.ReadOnly);
-const navItems = computed(() => [
-  { to: "/", icon: ChartPieIcon, label: "Overview", disabled: false },
-  { to: "/history", icon: ClockIcon, label: "History", disabled: false },
-  { to: "/receive", icon: DownloadIcon, label: "Receive", disabled: false },
-  { to: "/send", icon: SendIcon, label: "Send", disabled: readonly.value },
-  { to: "/dapps", icon: LayoutGridIcon, label: "DApps", disabled: false }
-]);
+const navItems = [
+  { to: "/", icon: ChartPieIcon, label: "Overview", disabled: ref(false) },
+  { to: "/history", icon: ClockIcon, label: "History", disabled: ref(false) },
+  { to: "/receive", icon: DownloadIcon, label: "Receive", disabled: ref(false) },
+  { to: "/send", icon: SendIcon, label: "Send", disabled: readonly },
+  { to: "/dapps", icon: LayoutGridIcon, label: "DApps", disabled: readonly }
+];
 </script>
 
 <template>
@@ -22,7 +22,7 @@ const navItems = computed(() => [
       v-for="item in navItems"
       :key="item.to"
       :to="item.to"
-      :class="{ 'pointer-events-none opacity-40': item.disabled }"
+      :class="{ 'pointer-events-none opacity-40': item.disabled.value }"
       active-class="text-primary cursor-default"
       class="text-muted-foreground hover:text-primary active:text-primary grow py-4 outline-hidden transition-colors focus:outline-hidden active:outline-hidden"
     >
