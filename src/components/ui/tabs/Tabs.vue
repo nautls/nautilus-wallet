dfe61b7e1f4a94909a8ffc73500bc75391ec6abb
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-17 14:31:36 -0300

    fix types

--- src/components/ui/tabs/Tabs.vue
+++ src/components/ui/tabs/Tabs.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts" generic="T extends string">
+<script setup lang="ts" generic="T extends string | number">
 import type { TabsRootEmits, TabsRootProps } from 'reka-ui'
 import { TabsRoot, useForwardPropsEmits } from 'reka-ui'
 
