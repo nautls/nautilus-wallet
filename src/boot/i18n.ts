1c9a064a5aba7a3ed8618538f95ef69afa56fc14
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-21 22:30:19 +0100

    improve type inference

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,30 +1,29 @@
 import { nextTick } from "vue";
 import { createI18n } from "vue-i18n";
-import type { I18n, I18nOptions } from "vue-i18n";
-import type EnglishMessages from "../locales/en.json";
+import type { I18n, Locale } from "vue-i18n";
+import type EnglishMessages from "@/locales/en.json";
 
 // import en locale dynamically to avoid the following vite warning:
 //
 // en.json is dynamically imported by i18n.ts but also statically imported
 // by i18n.ts, dynamic import will not move module into another chunk.
 const en = await importLocale("en");
+
+// Change the following line when adding more locales
 export const SUPPORTED_LOCALES = ["en", "pt"] as const;
 
-export type Locale = (typeof SUPPORTED_LOCALES)[number];
 export type MessageSchema = typeof EnglishMessages;
 
 type LocaleData = Record<string, unknown>;
 let i18nInstance: I18n<LocaleData, LocaleData, LocaleData, Locale, false>;
 
 export function setupI18n() {
-  const opt = {
+  return (i18nInstance = createI18n({
     legacy: false,
     locale: "en",
     fallbackLocale: "en",
     messages: { en }
-  } satisfies I18nOptions;
-
-  return (i18nInstance = createI18n(opt));
+  }));
 }
 
 export function getLocale(): Locale {
@@ -47,5 +46,5 @@ async function loadLocale(locale: Locale) {
 }
 
 async function importLocale(locale: Locale) {
-  return import(`../locales/${locale}.json`).then((r) => r.default || r);
+  return import(`@/locales/${locale}.json`).then((r) => r.default || r);
 }
--- src/types/d.ts/i18n.d.ts
+++ src/types/d.ts/i18n.d.ts
@@ -1,12 +1,13 @@
 // Interfaces need to imported first to be extended
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { DefineDateTimeFormat, DefineLocaleMessage, DefineNumberFormat } from "vue-i18n";
-import type { MessageSchema } from "@/boot/i18n";
+import type { MessageSchema, SUPPORTED_LOCALES } from "@/boot/i18n";
 
 declare module "vue-i18n" {
   // define the locale messages schema
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefineLocaleMessage extends MessageSchema {}
+  export type Locale = (typeof SUPPORTED_LOCALES)[number];
 
   // define the datetime format schema
   //   export interface DefineDateTimeFormat {
