ed7843cc3a29bb2ba4f9cf4d9c9474f7253bcca0
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-21 10:16:33 +0100

    refactor i18n handling functions

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,27 +1,25 @@
 import { nextTick } from "vue";
 import { createI18n } from "vue-i18n";
 import type { I18n, I18nOptions, Locale } from "vue-i18n";
-import en from "../locales/en.json";
+import type English from "../locales/en.json";
+
+// import en locale dynamically to avoid the following vite warning:
+//
+// en.json is dynamically imported by i18n.ts but also statically imported
+// by i18n.ts, dynamic import will not move module into another chunk.
+const en = await importLocale("en");
 
 type NonLegacyI18n = I18n<
-  { en: typeof en },
+  { en: typeof English },
   Record<string, unknown>,
   Record<string, unknown>,
   Locale,
   false
 >;
 
-export const SUPPORT_LOCALES = ["en", "pt"] as const;
+export const SUPPORTED_LOCALES = ["en", "pt"] as const;
 
 let i18nInstance: NonLegacyI18n;
-
-export function getLocale(): Locale {
-  return i18nInstance.global.locale.value;
-}
-
-export function setLocale(locale: Locale): void {
-  i18nInstance.global.locale.value = locale;
-}
 
 export function setupI18n() {
   const opt = {
@@ -34,17 +32,25 @@ export function setupI18n() {
   return (i18nInstance = createI18n(opt));
 }
 
-export function setI18nLanguage(locale: Locale): void {
-  setLocale(locale);
+export function getLocale(): Locale {
+  return i18nInstance.global.locale.value;
+}
+
+export async function setLocale(locale: Locale): Promise<void> {
+  if (!(locale in i18nInstance.global.messages)) {
+    await loadLocale(locale);
+  }
+
+  i18nInstance.global.locale.value = locale;
   document.querySelector("html")?.setAttribute("lang", locale);
 }
 
-export async function loadLocaleMessages(locale: Locale) {
-  // load locale messages
-  const messages = await import(`../locales/${locale}.json`).then((r) => r.default || r);
-
-  // set locale and locale message
+async function loadLocale(locale: Locale) {
+  const messages = await importLocale(locale);
   i18nInstance.global.setLocaleMessage(locale, messages);
-
   return nextTick();
+}
+
+async function importLocale(locale: Locale) {
+  return import(`../locales/${locale}.json`).then((r) => r.default || r);
 }
