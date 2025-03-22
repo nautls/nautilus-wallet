4666d2565490bc64405246878657bd014ff07d8a
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-22 13:17:00 +0100

    add i18n lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
 
 import jsPlugin from "@eslint/js";
+import vueI18n from "@intlify/eslint-plugin-vue-i18n";
 import tsParser from "@typescript-eslint/parser";
 import vuePlugin from "eslint-plugin-vue";
 import tsPlugin from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
 
-/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
   {
     ignores: ["**/node_modules", "**/dist", "**/coverage", "**/docs", "src/components/ui"]
   },
-  jsPlugin.configs.recommended,
+  ...vueI18n.configs.recommended,
   ...tsPlugin.configs.recommended,
   ...vuePlugin.configs["flat/essential"],
+  jsPlugin.configs.recommended,
   {
     languageOptions: {
       globals: {
@@ -37,7 +38,17 @@ export default [
       "no-console": "error",
       "no-undef": "off", // TypeScript's compiler already enforces this check. https://eslint.org/docs/latest/rules/no-undef#handled_by_typescript
       "@typescript-eslint/explicit-module-boundary-types": "off",
-      "sort-imports": "off"
+      "sort-imports": "off",
+      "@intlify/vue-i18n/no-deprecated-i18n-component": "off"
+    },
+
+    settings: {
+      "vue-i18n": {
+        localeDir: "src/locales/*.json",
+        // Specify the version of `vue-i18n` you are using.
+        // If not specified, the message will be parsed twice.
+        messageSyntaxVersion: "^11.0.0"
+      }
     }
   }
 ];
