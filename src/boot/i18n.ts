3d4ec47509c6fab5b7944051a7c18e5cee7971fd
arobsn <87387688+arobsn@users.noreply.github.com>
2025-03-28 21:50:01 +0100

    refactor i18n setup to use Composer and improve locale handling

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,12 +1,11 @@
 import { nextTick } from "vue";
 import { createI18n } from "vue-i18n";
-import type { I18n, Locale } from "vue-i18n";
+import type { Composer, I18n, Locale } from "vue-i18n";
 import type EnglishMessages from "@/locales/en.json";
 
-// import en locale dynamically to avoid the following vite warning:
-//
-// en.json is dynamically imported by i18n.ts but also statically imported
-// by i18n.ts, dynamic import will not move module into another chunk.
+// import en locale dynamically to avoid the following vite warning: "en.json
+// is dynamically imported by i18n.ts but also statically imported by i18n.ts,
+// dynamic import will not move module into another chunk."
 const en = await importLocale("en");
 
 // Change the following line when adding more locales
@@ -19,45 +18,39 @@ export type MessageSchema = typeof EnglishMessages;
 export type Translator = NonLegacyI18n["global"]["t"];
 export type DateFormatter = NonLegacyI18n["global"]["d"];
 
-let i18nInstance: NonLegacyI18n;
+let _globalInstance: Composer;
 
 export function setupI18n() {
-  i18nInstance = createI18n({
+  const i18n = createI18n({
     legacy: false,
     locale: "en",
     fallbackLocale: "en",
     messages: { en },
     datetimeFormats: {
-      en: {
-        long: {
-          dateStyle: "long"
-        },
-        short: {
-          dateStyle: "short"
-        }
-      }
+      en: { long: { dateStyle: "long" }, short: { dateStyle: "short" } }
     }
   });
 
-  return i18nInstance;
+  _globalInstance = i18n.global as Composer;
+  return i18n;
 }
 
 export function getLocale(): Locale {
-  return i18nInstance.global.locale.value as Locale;
+  return _globalInstance.locale.value as Locale;
 }
 
 export async function setLocale(locale: Locale): Promise<void> {
-  if (!(locale in i18nInstance.global.messages)) {
+  if (!(locale in _globalInstance.messages)) {
     await loadLocale(locale);
   }
 
-  i18nInstance.global.locale.value = locale;
+  _globalInstance.locale.value = locale;
   document.querySelector("html")?.setAttribute("lang", locale);
 }
 
 async function loadLocale(locale: Locale) {
   const messages = await importLocale(locale);
-  i18nInstance.global.setLocaleMessage(locale, messages);
+  _globalInstance.setLocaleMessage(locale, messages);
   return nextTick();
 }
 
