bb5404f9084b600fc5f35e1f0242c5d8fc686718
arobsn <87387688+arobsn@users.noreply.github.com>
2025-04-22 14:15:30 -0300

    clean up

--- src/types/d.ts/i18n.d.ts
+++ src/types/d.ts/i18n.d.ts
@@ -8,24 +8,4 @@ declare module "vue-i18n" {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefineLocaleMessage extends MessageSchema {}
   export type Locale = (typeof SUPPORTED_LOCALES)[number];
-
-  // define the datetime format schema
-  //   export interface DefineDateTimeFormat {
-  //     short: {
-  //       hour: "numeric";
-  //       minute: "numeric";
-  //       second: "numeric";
-  //       timeZoneName: "short";
-  //       timezone: string;
-  //     };
-  //   }
-
-  // define the number format schema
-  //   export interface DefineNumberFormat {
-  //     currency: {
-  //       style: "currency";
-  //       currencyDisplay: "symbol";
-  //       currency: string;
-  //     };
-  //   }
 }
