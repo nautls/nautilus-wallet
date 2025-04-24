c1ac95cabf6738256a5e778cac3ffc9c8aa6adb9
c8e4 <c8e4@proton.me>
2025-04-24 20:48:59 +0200

    add ru-RU

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -10,7 +10,7 @@ const enUS = await importLocale("en-US");
 
 // Supported languages must be ordered by the most used to the least used.
 // See https://www.ethnologue.com/insights/ethnologue200/ for reference.
-export const SUPPORTED_LOCALES = ["en-US", "pt-BR", "id-ID"] as const;
+export const SUPPORTED_LOCALES = ["en-US", "pt-BR", "id-ID", "ru-RU"] as const;
 const DEFAULT_LOCALE = "en-US" as const;
 
 // Labels must be in the target language following the format: "Language (Country)", where
@@ -19,7 +19,8 @@ const DEFAULT_LOCALE = "en-US" as const;
 export const LANGUAGE_LABELS = new Map<Locale, string>([
   ["en-US", "English (US)"], //         English (United States)
   ["pt-BR", "Português (BR)"], //       Portuguese (Brazil)
-  ["id-ID", "Bahasa Indonesia (ID)"] // Indonesian (Indonesia)
+  ["id-ID", "Bahasa Indonesia (ID)"], // Indonesian (Indonesia)
+  ["ru-RU", "Русский (RU)"] //          Russian (Russia)
 ]);
 
 export type MessageSchema = typeof EnglishMessages;
